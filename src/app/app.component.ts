fb4e3262203bd8ae599419993eb7d26774b0f462
Elena Vonder Meulen <evonder13@gmail.com>
2020-11-10 15:42:44 -0500

    added styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  btnStyle: string = "btn btn-primary";
   students: any [];
   studentCollection: any[] = [
     {name: "Tonya", favNumber: 743747, favColor: "Green"},
-    {name: "Jack", favNumber: 32842, favColor: "Red"},
     {name: "Kali", favNumber: 239753, favColor: "Black"},
     {name: "Hung", favNumber: 3492735, favColor: "Grey"},
+    {name: "Jack", favNumber: 32842, favColor: "Red"},
     {name: "Mark", favNumber: 34523, favColor: "Grey"},
     {name: "Christian", favNumber: 4, favColor: "Black"},
     {name: "Johnathan", favNumber: 74532, favColor: "Yellow"},
     {name: "Manami", favNumber: 294773, favColor: "Blue"},
     {name: "Tracy", favNumber: 54854, favColor: "Pink"},
+    {name: "Elena", favNumber: 8766, favColor: "White"},
+    {name: "Sarah", favNumber: 23835, favColor: "Red"},
     {name: "Marc", favNumber: 735345, favColor: "Green"},
-    {name: "Greg", favNumber: 987324, favColor: "Green"},
-    {name: "Elena", favNumber: 8766, favColor: "White"}
+    {name: "Greg", favNumber: 987324, favColor: "Forest Green"}
   ];
 
   title = 'student-list';
 
   getStudents(): void {
     this.students = this.studentCollection;
+  }
+
+  changeColor(): void{
+    this.btnStyle = "btn btn-danger";
   }
 }
