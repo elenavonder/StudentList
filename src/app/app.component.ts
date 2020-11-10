a7e566439f4560627757031b1023a473482204ca
Elena Vonder Meulen <evonder13@gmail.com>
2020-11-10 11:20:21 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  students: any[] = [
+    {name: "Tonya", favNumber: 743747, favColor: "Green"},
+    {name: "Jack", favNumber: 32842, favColor: "Red"},
+    {name: "Kali", favNumber: 239753, favColor: "Black"},
+    {name: "Hung", favNumber: 3492735, favColor: "Grey"},
+    {name: "Mark", favNumber: 34523, favColor: "Grey"},
+    {name: "Christian", favNumber: 4, favColor: "Black"},
+    {name: "Johnathan", favNumber: 74532, favColor: "Yellow"},
+    {name: "Manami", favNumber: 294773, favColor: "Blue"},
+    {name: "Tracy", favNumber: 54854, favColor: "Pink"},
+    {name: "Marc", favNumber: 735345, favColor: "Green"},
+    {name: "Greg", favNumber: 987324, favColor: "Green"},
+    {name: "Elena", favNumber: 8766, favColor: "White"}
+  ];
+
   title = 'student-list';
 }
